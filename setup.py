cbe64e5a20ea2a8b704c1abd1de53ef056e2c5bc
devmatteini <dev.matteini@gmail.com>
2019-05-29 18:34:16 +0200

    Update .gitignore for production. Add setup.py used to publish on pypi.org

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="forecastpy",
+    version="1.0.0",
+    author="Cosimo Matteini",
+    author_email="dev.matteini@gmail.com",
+    description="A python module to interact with the OpenWeatherAPI in a simple and fast way.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/devmatteini/forecastpy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    keywords = "weather forecast openweathermap"
+)
